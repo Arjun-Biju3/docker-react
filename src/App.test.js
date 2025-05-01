156e1bcefb065aca5665e4b49a807f6d1524406d
Arjun Biju <arjunbiju332@gmail.com>
2025-05-01 04:17:03 +0000

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; // optional but recommended for extended matchers
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
